a3c4f3d119658ceec53194ef6f62f27de84a9e1f
matty-y-ice <matty_ice_2011@pm.me>
2025-03-17 22:10:27 -0400

    added goldbach function

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,4 @@
-let find_primes_eratosthenes n =
+let sieve_eratosthenes n =
   let arr = Array.make (n + 1) true in
   arr.(0) <- false ;
   arr.(1) <- false ;
@@ -12,8 +12,20 @@ let find_primes_eratosthenes n =
   done ;
   arr
 
-let primes n =
-  let arr = find_primes_eratosthenes n in
+let get_primes n =
+  let arr = sieve_eratosthenes n in
   let result = ref [] in
   Array.iteri (fun i is_prime -> if is_prime then result := i :: !result) arr ;
   List.rev !result
+
+let goldbach value =
+  let result = ref [] in
+  if value >= 4 && value mod 2 = 0 then
+    let primes = get_primes value in
+    List.iter (fun p1 ->
+      List.iter (fun p2 ->
+      if p1 + p2 = value && p1 <= then
+      result := (p1, p2) :: !result
+      ) primes
+      ) primes ;
+    !result
--- flake.nix
+++ flake.nix
@@ -9,10 +9,11 @@
         buildInputs = [
           nixpkgs.legacyPackages.x86_64-linux.ocaml
           nixpkgs.legacyPackages.x86_64-linux.dune_3
-          nixpkgs.legacyPackages.x86_64-linux.ocamlPackages.merlin
+          nixpkgs.legacyPackages.x86_64-linux.opam
           nixpkgs.legacyPackages.x86_64-linux.ocamlPackages.ocaml-lsp
           nixpkgs.legacyPackages.x86_64-linux.ocamlPackages.utop
           nixpkgs.legacyPackages.x86_64-linux.ocamlPackages.ocamlformat
+          nixpkgs.legacyPackages.x86_64-linux.ocamlPackages.ocp-indent
           nixpkgs.legacyPackages.x86_64-linux.binutils
         ];
       };
